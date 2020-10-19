3c6bda4acc6315ad40fc994826e09a3606803cc2
Matt Simmons <bandman@gmail.com>
2020-10-19 05:48:29 +0000

    Location services started

--- twitter_interface.py
+++ twitter_interface.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+import sys
 import twitter
 import read_config
 import json
 from urllib.parse import quote as urlquote
+import geograpy
+import geocoder
 
 def twit_init():
     s = read_config.get_secrets()
@@ -60,16 +63,55 @@ def find_tweet_geo(tweet):
     # Things we care about: 
     print("Tweet Coordinates: %s" % str(tweet.coordinates))
     print("Tweet language: %s" % str(tweet.lang))
+
     print("User Location: %s" % str(tweet.user.location))
+    # user location is freetext, so we will scrape it for location
+    if tweet.user.location is not None:
+        #TODO: check for coords
+
+        tweet_coords = dict()
+        # Use geograpy
+        try:
+            p = geograpy.Extractor(text = tweet.user.location)
+        except Exception as err:
+            print("... problem looking up location %s: %s" % (tweet.user.location, str(err)))
+            return
+        p.find_entities()
+        loc = geograpy.Locator()
+        city = loc.locateCity(p.places)
+        if city:
+            try:
+                location_name = "%s, %s, %s" % (str(city.name), str(city.region.name), str(city.country.name))
+                g = geocoder.arcgis(location_name)
+                tweet_coords = {"lat": g.current_result.lat, "lon": g.current_result.lng} 
+                # print("DEBUG: location_name: %s" % location_name)
+                # print("DEBUG: %s" % str(tweet_coords))
+            except Exception as err:
+                print("... problem geocoding %s: %s" % (str(city), str(err)))
+                location_name = "Unknown"
+        else:
+            location_name = "Unknown"
+        if tweet_coords.get('lat') is None or tweet_coords.get('lon') is None:
+            tweet_coords['lat'] = "0"
+            tweet_coords['lon'] = "0"
+            location_name = "Unknown"
+        print("Location: %s (%s,%s)" % (location_name, tweet_coords['lat'], tweet_coords['lon']))
     print("User Timezone: %s" % str(tweet.user.time_zone))
     pass
 
 if __name__ == '__main__':
     twit = twit_init()
-    tweet = twit.GetHomeTimeline(count=1)[0]
-    print(tweet.AsJsonString())
-    print("-=-=-")
-    print("%s - https://twitter.com/%s/status/%s" % (tweet.user.screen_name, tweet.user.screen_name, tweet.id))
-    print(tweet.text)
-    find_tweet_geo(tweet)
+    try:
+        #tweet = twit.GetHomeTimeline(count=1)[0]
+        tweets = search(twit, sys.argv[1], count=10)
+    except twitter.error.TwitterError as err:
+        print("Error: %s" % str(err))
+        pass
+    #print(tweet.AsJsonString())
+    for tweet in tweets:
+        print(" ")
+        print("-=-=-")
+        print("%s - https://twitter.com/%s/status/%s" % (tweet.user.screen_name, tweet.user.screen_name, tweet.id))
+        print(tweet.text)
+        find_tweet_geo(tweet)
 
